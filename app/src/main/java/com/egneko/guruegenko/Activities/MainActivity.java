ab29a5d8098c8d93d734d3d1cd821ef95992fd22
Suman Neupane <sumanmyon.sam@gmail.com>
2019-07-11 19:55:21 +0545

    Adding material colors.

--- app/src/main/java/com/egneko/guruegenko/Activities/MainActivity.java
+++ app/src/main/java/com/egneko/guruegenko/Activities/MainActivity.java
@@ -1,7 +1,9 @@
-package com.egneko.guruegenko;
+package com.egneko.guruegenko.Activities;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+
+import com.egneko.guruegenko.R;
 
 public class MainActivity extends AppCompatActivity {
 
